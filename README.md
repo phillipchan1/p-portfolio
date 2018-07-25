b8ea77d51eddd325881f3b1f6a8f2c23ed82f857
phillipchan2 <phillip.chan@liferay.com>
2018-07-25 16:36:40 -0700

    Add Live Reload Dev Server and Readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Portfolio Site
+
+## Requirements:
+- PHP 5.4.0+.
+- Node 4+
+
+## Start Development
+
+1. Install dependencies
+
+```
+npm install
+```
+
+2. Start development server:
+
+```
+gulp
+
+// or "npx gulp" if you dont have it globally installed
+```
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect-php');
+const browserSync = require('browser-sync');
+
+gulp.task('connect-sync', function() {
+  connect.server({}, function (){
+    browserSync({
+      proxy: '127.0.0.1:8000'
+    });
+  });
+
+  gulp.watch('**/*.php').on('change', function () {
+    browserSync.reload();
+  });
+});
+
+gulp.task('default', ['connect-sync']);
