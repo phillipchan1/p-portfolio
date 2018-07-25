cb27025bb10595ac653b41696a151319764a420c
phillipchan2 <phillip.chan@liferay.com>
2018-07-25 15:42:35 -0700

    Services start + SF

---
+++ services/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<?php include '../partials/head.php';?>
+
+<body>
+    <?php include '../partials/header.php';?>
+
+
+
+    <?php include '../partials/footer.php';?>
+</body>
+
+</html>
