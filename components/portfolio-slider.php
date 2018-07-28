2e34ea0d968e6b741fbff02cff30f58b2c45379c
phillipchan2 <phillip.chan@liferay.com>
2018-07-28 15:49:52 -0700

    SF + Update portfolio items

--- components/portfolio-slider.php
+++ components/portfolio-slider.php
@@ -1,101 +1,68 @@
 <div class="swiper-wrapper">
-		<div class="swiper-slide type-1">
-			<div class="whole item" id="work-1">
-				<div class="work-caption">
-					<h3>Liferay.com</h3>
-					<p class="work-client">Website Rebrand</p>
-				</div>
-				<a href="/portfolio/liferay">view more</a>
+	<div class="swiper-slide type-1">
+		<div class="whole item" id="work-1">
+			<div class="work-caption">
+				<h3>Liferay.com</h3>
+				<p class="work-client">Website Build</p>
 			</div>
+			<a href="/portfolio/liferay">view more</a>
 		</div>
-		<div class="swiper-slide type-3">
-			<div class="top-section item" id="work-2">
-				<div class="work-caption">
-					<h3>Hubspot Microservice</h3>
-					<p class="work-client">Node App</p>
-				</div>
-				<a href="/portfolio/hubspot-microservice">view more</a>
+	</div>
+	<div class="swiper-slide type-3">
+		<div class="top-section item" id="work-2">
+			<div class="work-caption">
+				<h3>Hubspot Microservice</h3>
+				<p class="work-client">Node App</p>
 			</div>
-			<div class="bottom-section item" id="work-3">
-				<div class="work-caption">
-					<h3>Liferay.com Documentation</h3>
-					<p class="work-client">Node / Angular / Gulp App</p>
-				</div>
-				<a href="/portfolio/liferay-documentation">view more</a>
-			</div>
+			<a href="/portfolio/hubspot-microservice">view more</a>
 		</div>
-		<div class="swiper-slide type-2">
-			<div class="top-section item" id="work-4">
-				<div class="work-caption">
-					<h3>Liferay Design</h3>
-					<p class="work-client">React Website</p>
-				</div>
-				<a href="/portfolio/liferay-design">view more</a>
+		<div class="bottom-section item" id="work-3">
+			<div class="work-caption">
+				<h3>Liferay.com Documentation</h3>
+				<p class="work-client">Node / Angular / Gulp App</p>
 			</div>
-			<div class="bottom-section item" id="work-5">
-				<div class="work-caption">
-					<h3>OneWest Bank</h3>
-					<p class="work-client">Website Rebrand</p>
-				</div>
-				<a href="/portfolio/onewestbank">view more</a>
-			</div>
+			<a href="/portfolio/liferay-documentation">view more</a>
 		</div>
-		<div class="swiper-slide type-1">
-			<div class="whole item" id="work-6">
-				<div class="work-caption">
-					<h3>phillipchan.org</h3>
-					<p class="work-client">Wordpress Theme</p>
-				</div>
-				<a href="/portfolio/phillipchanorg">view more</a>
+	</div>
+	<div class="swiper-slide type-2">
+		<div class="top-section item" id="work-4">
+			<div class="work-caption">
+				<h3>Liferay Design</h3>
+				<p class="work-client">React Website</p>
 			</div>
+			<a href="/portfolio/liferay-design">view more</a>
 		</div>
-		<div class="swiper-slide type-4">
-			<div class="top-section item" id="work-7">
-				<div class="work-caption">
-					<h3>Jetly</h3>
-					<p class="work-client">Angular App</p>
-				</div>
-				<a href="/portfolio/jetly">view more</a>
+		<div class="bottom-section item" id="work-5">
+			<div class="work-caption">
+				<h3>OneWest Bank</h3>
+				<p class="work-client">Website Build</p>
 			</div>
-			<div class="bottom-section item" id="work-8">
-				<div class="work-caption">
-					<h3>Liferay Email Builder</h3>
-					<p class="work-client">Node App</p>
-				</div>
-				<a href="/portfolio/liferay-email-builder">view more</a>
-			</div>
+			<a href="/portfolio/onewestbank">view more</a>
 		</div>
-		<div class="swiper-slide type-1">
-			<div class="whole item" id="work-9">
-				<div class="work-caption">
-					<h3>Attitudes</h3>
-					<p class="work-client">Attitudes.com</p>
-				</div>
-				<a href="/portfolio/">view more</a>
+	</div>
+	<div class="swiper-slide type-1">
+		<div class="whole item" id="work-6">
+			<div class="work-caption">
+				<h3>phillipchan.org</h3>
+				<p class="work-client">Wordpress Theme</p>
 			</div>
+			<a href="/portfolio/phillipchanorg">view more</a>
 		</div>
-		<div class="swiper-slide type-1">
-			<div class="whole item" id="work-10">
-				<div class="work-caption">
-					<h3>New arrivals</h3>
-					<p class="work-client">life&fashion</p>
-				</div>
-				<a href="/portfolio/">view more</a>
+	</div>
+	<div class="swiper-slide type-4">
+		<div class="top-section item" id="work-7">
+			<div class="work-caption">
+				<h3>Jetly</h3>
+				<p class="work-client">Angular 5 App</p>
 			</div>
+			<a href="/portfolio/jetly">view more</a>
 		</div>
-		<div class="swiper-slide type-2">
-			<div class="top-section item" id="work-11">
-				<div class="work-caption">
-					<h3>Years</h3>
-					<p class="work-client">Distinguish.com</p>
-				</div>
-				<a href="/portfolio/">view more</a>
+		<div class="bottom-section item" id="work-8">
+			<div class="work-caption">
+				<h3>Liferay Email Builder</h3>
+				<p class="work-client">Node App</p>
 			</div>
-			<div class="bottom-section item" id="work-12">
-				<div class="work-caption">
-					<h3>Power</h3>
-					<p class="work-client">Showing.com</p>
-				</div>
-				<a href="/portfolio/">view more</a>
-			</div>
+			<a href="/portfolio/liferay-email-builder">view more</a>
 		</div>
+	</div>
+</div>
