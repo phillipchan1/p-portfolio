32c0d23a284139f0f47d4157c60d2796959c17a1
phillipchan2 <phillip.chan@liferay.com>
2018-08-04 14:26:11 -0700

    Add google analytics + SF:

--- partials/head.php
+++ partials/head.php
@@ -1,19 +1,30 @@
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <title>PCHAN - The Portfolio of Phillip Chan</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>PCHAN - The Portfolio of Phillip Chan</title>
 
-    <link rel="shortcut icon" type="image/png" href="/assets/images/favicon.png"/>
-    <link rel="stylesheet" href="/assets/static/css/foundation.css">
-    <link rel="stylesheet" href="/assets/static/css/slick.css">
-    <link rel="stylesheet" href="/assets/static/css/slick-theme.css">
-    <link rel="stylesheet" href="/assets/static/css/swiper.min.css">
-    <link rel="stylesheet" href="/assets/static/css/app.css">
-    <link rel="stylesheet" href="/assets/static/css/responsive.css">
-    <link href="/assets/static/css/fontello.css" media="screen" rel="stylesheet" type="text/css" />
-    <link href="/assets/static/css/font-awesome.css" media="screen" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="/assets/static/css/linea-styles.css" />
-    <link rel="stylesheet" href="/assets/static/css/linea-arrows-styles.css" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="/assets/images/favicon.png"/>
+	<link rel="stylesheet" href="/assets/static/css/foundation.css">
+	<link rel="stylesheet" href="/assets/static/css/slick.css">
+	<link rel="stylesheet" href="/assets/static/css/slick-theme.css">
+	<link rel="stylesheet" href="/assets/static/css/swiper.min.css">
+	<link rel="stylesheet" href="/assets/static/css/app.css">
+	<link rel="stylesheet" href="/assets/static/css/responsive.css">
+	<link href="/assets/static/css/fontello.css" media="screen" rel="stylesheet" type="text/css" />
+	<link href="/assets/static/css/font-awesome.css" media="screen" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="/assets/static/css/linea-styles.css" />
+	<link rel="stylesheet" href="/assets/static/css/linea-arrows-styles.css" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123369263-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-123369263-1');
+  </script>
+
 </head>
