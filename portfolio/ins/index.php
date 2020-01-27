56c601266cc0a4630ed45219b9971c5420a6a859
phil chan <phillipchan1@gmail.com>
2020-01-27 14:44:58 -0800

    Add initial assets for ins

---
+++ portfolio/ins/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<?php include '../../partials/head.php';?>
+
+<body>
+	<?php include '../../partials/header.php';?>
+
+	<div class="case-header dark">
+		<div class="media-wrapper">
+			<video muted autoplay preload>
+				<source src='/portfolio/liferay/liferay-home-banner.mp4' type='video/mp4'>
+				</video>
+			</div>
+			<div class="large-12 columns">
+				<h2>A brave new world</h2>
+			</div>
+			<p class="scroll-down-arrow">
+				<span class="scroll-down">
+					<i class="icon-arrows-slim-down"></i>
+				</span>
+			</p>
+		</div>
+		<div class="case-study-content">
+			<div class="full no-right">
+				<div class="text-block-right">
+					<p class="big-text">
+						Finding itself at a crossroads, Liferay was going through an existential crisis having evolved from a successful startup to now a multinational enterprise grossing over 100m in sales a year. It was now desperate to find its focus in its identity of being a leader in the digital transformation platform
+					</p>
+					<p>
+						This project conglomerated several initiatives which was to 1) introduce the new Liferay 7.1 product 2) Introduce Liferay commerce and 3) Rebrand itself as a relevant player in the field by providing clarity as a 3 product company–Liferay Portal, Liferay Commerce, and Analytics Cloud.
+					</p>
+					<div class="three spacing"></div>
+					<div class="row">
+						<div class="large-5 columns">
+							<div class="spacing"></div>
+							<h3 class="meta-title">client</h3>
+							<p class="meta-data">Liferay</p>
+						</div>
+						<div class="large-7 columns">
+							<div class="spacing"></div>
+							<h3 class="meta-title">Main roles</h3>
+							<p class="meta-data">Lead Developer</p>
+						</div>
+					</div>
+					<div class="spacing"></div>
+					<div class="row">
+						<div class="large-5 columns">
+							<h3 class="meta-title">Category</h3>
+							<p class="meta-data">Website Rebuild</p>
+						</div>
+						<div class="large-7 columns">
+							<h3 class="meta-title">Project timeline</h3>
+							<p class="meta-data">8 weeks</p>
+						</div>
+					</div>
+					<div class="spacing"></div>
+					<div class="row">
+						<div class="large-5 columns">
+							<h3 class="meta-title">Technologies</h3>
+							<p class="meta-data">Freemarker, Javascript, Java, HTML, SASS, Liferay, Propietary Atomic Component Build Tool</p>
+						</div>
+					</div>
+					<div class="four spacing"></div>
+					<p>
+						<a href="https://www.liferay.com" target="_blank" class="boxed button black launch">Launch project <i class="icon-arrows-slim-right"></i> </a>
+					</p>
+				</div>
+			</div>
+			<div class="full">
+				<div class="swiper-container" id="office-images">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide">
+							<img src="/portfolio/liferay/animating cards.gif" alt="">
+						</div>
+						<div class="swiper-slide">
+							<img src="/portfolio/liferay/dxp.png" alt="">
+						</div>
+						<div class="swiper-slide">
+							<img src="/portfolio/liferay/click-carousel.png" alt="">
+						</div>
+					</div>
+					<div class="swiper-pagination"></div>
+				</div>
+				<!-- Add Arrows -->
+				<div class="swiper-arrows">
+					<div class="swiper-button-next office"></div>
+					<div class="swiper-button-prev office"></div>
+				</div>
+			</div>
+			<div class="full no-right">
+				<div class="text-block-right">
+					<h3>The challenge.</h3>
+					<p class="big-text">The challenge with rebuilding websites is navigating existing architecture and new architecture. Existing architecture served its purpose but remains critical as the transitional foundation for new entities.</p>
+
+					<p>The new architecture foreshadows direction and possibilities. The challenge with rebuildling Liferay.com was figuring out a way to create new cutting edge UI, integrating a design system and language into programmable pieces, and delivering the results quickly for its Liferay’s new birth. </p>
+					<div class="four spacing"></div>
+					<h3>The solution.</h3>
+					<p class="big-text">With Liferay’s rebirth came a maturation of its UX teams to deliver UI components down into a predictable science so that developers could predictably turn UI components into living components.  </p>
+					<p>Leveraging a UI dictionary built on Figma, as well as building a brand new proprietary build tool, I led the charge in defining specifications for the building blocks for Liferay’s new visual identity. I crafted each page using concentric building blocks of atoms, molecules, organisms that matched design specs in a way that both delivered business value while creating an architecture that would support our visual evolution for years to come.</p>
+
+					<div class="four spacing"></div>
+					<div id="process-hint">
+						<div class="action-hint">Click & drag to explore.</div>
+						<div class="action-hint-for-touch">Swipe to explore.</div>
+					</div>
+				</div>
+				<div class="swiper-container" id="process">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide">
+							<h3>01. Maturation of UX</h3>
+							<p>The UX team grew into thinking more critically about how components are organized and deployed</p>
+						</div>
+						<div class="swiper-slide">
+							<h3>02. Component Build Tool</h3>
+							<p>Liferay's propietary build tool allows added power to an aging technology Freemarker.</p>
+						</div>
+						<div class="swiper-slide">
+							<h3>03. Atomic Components Builds</h3>
+							<p>Merging a development and design language, we launched components using the same dictionary as the UX team</p>
+						</div>
+						<div class="swiper-slide">
+							<h3>04. Timely Delivery</h3>
+							<p>The rebrand launched successfully with all global business units cheering its returns for generating leads</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="full light-grey">
+				<div class="large-7 columns">
+					<h3>The result.</h3>
+					<p class="big-text">Liferay finds new footing in a world rife with competition. It peeks its head out with confidence knowing its presentation now matches its worth.</p>
+				</div>
+			</div>
+			<div class="full red colored-bg">
+				<p class="centered-text">
+					<img src="/portfolio/liferay/responsive.png" alt="">
+				</p>
+			</div>
+			<!-- <div class="full">
+				<div class="large-7 columns large-centered">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet cumque quidem nulla officia consequuntur, eligendi repellat, laborum perferendis nihil, debitis officiis consectetur tenetur quo eaque quam expedita quibusdam atque inventore?</p>
+				</div>
+			</div>
+			<div class="full purple colored-bg">
+				<p class="centered-text">
+					<img src="/portfolio/liferay/laptop.jpg" alt="">
+				</p>
+			</div>
+			<div class="full no-right white">
+				<div class="text-block-right">
+					<h3>Our client is happy.</h3>
+					<div class="testimonials">
+						<blockquote class="big-text">
+							"Lorem ipsum dolor sit amet, perferendis tempora ratione dolore, maiores ea culpa optio animi deleniti. Quis quos cum illo quod sapiente dolorum, eveniet praesentium."
+						</blockquote>
+						<p>- Name, position</p>
+					</div>
+				</div>
+			</div> -->
+
+			<!-- Work with us -->
+			<?php include '../../components/work-with-us.php';?>
+
+			<?php include '../../components/back-to-cases.php';?>
+		</div>
+
+		<?php include '../../partials/footer.php';?>
+	</body>
+
+	</html>
